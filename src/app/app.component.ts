2e160f264a8f74fd7101d5900a1dcd8f24fc4164
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-20 18:01:29 +0530

    creating custom observalble

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription, interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  time: Observable<number>;
+  intervalSubscription!: Subscription;
+
+  constructor(){
+    this.time =interval(1000).pipe(take(50));
+  }
   title = 'angularLocalStorageCrud';
+
+  ngOnInit() {
+    let customObservable = new Observable((observer : Observer<any>)=>{
+      let count = 0;
+       setInterval(()=>{
+         observer.next(count);
+         count++;
+       },1000)
+     });
+     
+     this.intervalSubscription = customObservable.subscribe((val)=>{
+      console.log(val);
+    })
+  
+  }
+
+
+
+
 }
