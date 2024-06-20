036de86c427f993df9655b645528b84003b7a579
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-20 18:35:23 +0530

    observable error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,33 +4,36 @@ import { Observable, Observer, Subscription, interval, take } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   time: Observable<number>;
   intervalSubscription!: Subscription;
 
-  constructor(){
-    this.time =interval(1000).pipe(take(50));
+  constructor() {
+    this.time = interval(1000).pipe(take(50));
   }
   title = 'angularLocalStorageCrud';
 
   ngOnInit() {
-    let customObservable = new Observable((observer : Observer<any>)=>{
+    let customObservable = new Observable((observer: Observer<any>) => {
       let count = 0;
-       setInterval(()=>{
-         observer.next(count);
-         count++;
-       },1000)
-     });
-     
-     this.intervalSubscription = customObservable.subscribe((val)=>{
-      console.log(val);
-    })
-  
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error('Count is greater than 3');
+        }
+        if (count > 2) {
+          observer.complete();
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.intervalSubscription = customObservable.subscribe({
+      next: (val) => console.log(val),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
   }
-
-
-
-
 }
